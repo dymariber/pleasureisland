71902fefdeb4158bc0898287b15df4154c29b10a
dymariber <101956056+dymariber@users.noreply.github.com>
2022-10-30 22:09:20 -0400

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,13 @@
 
+const hamburger= document.querySelector(".hamburger__icon");
+const closebtn= document.querySelector(".closebtn");
+
+hamburger.addEventListener("click", ()=> {
+    
+    document.getElementById("mobile__nav").style.width = "100%";
+});
+closebtn.addEventListener("click", ()=>{
+    document.getElementById("mobile__nav").style.width = "0%";
+});
+
+
